9d3379aa07688d67825710f75311a0e342287b05
michux-moo <michelleux3@gmail.com>
2025-08-19 19:51:58 -0700

    Fix react-dom import syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react', 'react-dom', 'scheduler'],
+    exclude: ['lucide-react'],
+    include: ['react-dom/client']
   },
 });
